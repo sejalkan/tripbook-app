f58f8733784a513eb84cad820a79b9a844e1efc5
sejal <sejal@student.chalmers.se>
2022-09-10 20:19:22 +0000

    Created basic schema design for place entity.

---
+++ server/schemas/place.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Place = new Schema({
+    placeType: String,
+    email_address: String,
+    password: String,
+    bio: String,
+    place_id: String,
+    placeName: String,
+    address: String,
+});
+
+module.exports = mongoose.model('Place', Place);
