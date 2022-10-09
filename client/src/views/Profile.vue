8296cc9a46b03a0423602a172a0a9eae66029df2
Sejal <guskanase@student.gu.se>
2022-10-09 17:40:35 +0200

    Profile page initial implementation

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,24 +1,49 @@
 <template>
-  <div class="profilePage" style="body">
-    <div class="form">
-    <h1> My Profile </h1>
-    <!-- <b-jumbotron header='Profile Page' lead='Nothing Yet' /> -->
-    <input type="button" class="editBtn" value="Edit"> <br> <br>
-    <label class="l1"> Username </label>
-    <input type="label" v-bind:value="currentUser.username" class="username" readonly> <br>
-    <label class="l1"> Email </label>
-    <input type="label" v-bind:value="currentUser.email" class="email" readonly> <br>
-    <label class="l1"> Profile Picture </label>
-    <img v-bind:src="currentUser.profilePicture" class="profilePicture" readonly> <br>
-    <label class="l1"> Bio </label>
-    <input type="label" v-bind:value="currentUser.bio" class="bio" readonly> <br> <br>
-    <a href="/edit" class="changePass"> Change password </a>
+  <div class="main">
+    <div class="information">
+    <h1>{{currentUser.username}}</h1>
+    <h2> Bio: <br> {{currentUser.bio}}</h2>
+    <button class="editBtn">Edit Profile </button>
+    </div>
+    <div style="padding: 2.5rem">
+      <b-tabs content-class="mt-3" align="center">
+        <b-tab title="Posts"><p>Your Posts</p>
+
+        <div class="row justify-content-center" style="padding: 2rem" v-for="post in posts" v-bind:key="post._id">
+      <b-card style="card" header-tag="header" footer-tag="footer" no-body>
+        <b-tabs card>
+          <template #tabs-start>
+            <li role="presentation" class="nav-item align-self-center"> @{{currentUser.username}} </li>
+          </template>
+        <b-tab title="Post">
+          <img src='@/assets/sepehr-moradian-XdtUEWzdU0A-unsplash.jpg' />
+        </b-tab>
+        <b-tab class="scroll" title="Reviews">
+          <b-card-text> <Post v-bind:post='post'/> </b-card-text>
+        </b-tab>
+        </b-tabs>
+        <template #footer>
+        Description
+      </template>
+      </b-card>
+      <p></p>
+    </div>
+
+        </b-tab>
+        <b-tab title="Followers"><p>Your Followers</p></b-tab>
+        <b-tab title="Following"><p>Your Following</p></b-tab>
+
+      </b-tabs>
     </div>
   </div>
 </template>
 
 <script>
+import Post from '../components/Post.vue'
+import { Api } from '@/Api'
+
 export default {
+  components: { Post },
   props: { currentUser: Object },
   created() {
     if (localStorage.getItem('token') === null) {
@@ -29,76 +54,101 @@ export default {
     if (localStorage.getItem('token')) {
       console.log(this.currentUser)
     }
+
+    Api.get('/posts')
+      .then(response => {
+        console.log(response.data)
+        this.posts = response.data.posts
+      })
+      .catch(error => {
+        console.error(error)
+        this.post = []
+      })
+      .then(() => {
+        console.log('This runs every time after success or error.')
+      })
+  },
+
+  data() {
+    return {
+      posts: []
+    }
   }
 }
+
 </script>
 
 <style scoped>
-body {
-    background: black;
+.header {
+  text-align: center;
+  font-weight: bold;
+  border-top: 0ch;
 }
-.profilePage{
+.main{
+  padding-top: 0px;
+}
+p {
+  font-weight: bold;
+  text-align: center;
+}
+
+img {
+  max-height: 600px;
+  max-width: 800px;
+}
+
+.card {
+  min-width: 800px;
+  min-height: 680px;
+}
+
+.scroll {
+  min-width: 800px;
+  min-height: 500px;
+  max-height: 500px;
+  max-width: 800px;
+  overflow-y: auto;
+  padding: 1rem;
+  margin-bottom: 3rem !important;
+}
+
+li {
+  padding-right: 1.5rem;
+  padding-left: 0.5rem;
+  font-weight: bold;
+}
+
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+
+h1 {
+  padding: 0rem;
+  padding-bottom: 1rem;
+  font-family:monospace;
+  font-style: normal;
+  margin: 0%;
+}
+
+h2{
+  text-align: left;
+}
+
+.information{
+  text-align: left;
+  padding-left: 0%;
+  margin:0;
   background-color: #f6eef0;
-  overflow-y: hidden;
-  height: 100vh;
-  padding: 5%;
 }
-.form{
-  height: 700px;
-  width: 50%;
-  padding: 25px;
-  padding-top: 50px;
-  background-color: #ffffff;
-  float: left;
 
-}
 .editBtn{
-  background-color: #c8b4d0;
-  border: none;
-  height: 40px;
-  width: 70px;
-  font-family: inter;
-  font-size: 16px;
-  margin-left: 20px;
-}
-
-h1{
-  margin-bottom: 0%;
-}
-
-.username{
-  border-color: #f6eef0;
-  border-style: double;
-  width: 50%;
-  height: 35px;
-}
-
-.bio{
-  border-color: #f6eef0;
-  border-style: double;
-  width: 50%;
-  height: 35px;
-}
-
-.email{
-  border-color: #f6eef0;
-  border-style: double;
-  width: 50%;
-  height: 35px;
-}
-
-.profilePicture{
-  border-color: #f6eef0;
-  border-style: double;
-}
-
-.l1{
-  color: grey;
-  margin: 20px;
-}
-
-.changePass{
-  margin: 20px;
-}
-
+        background-color:#c8b4d0;
+        border: none;
+        height: 50px;
+        width: 100px;
+        font-family: inter;
+        font-size: 16px;
+    }
 </style>
